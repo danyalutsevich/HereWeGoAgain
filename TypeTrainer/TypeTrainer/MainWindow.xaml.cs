32374b06164fbf35cd104e8f7a4ad878ebe0e239
danyalutsevich <luchevich31@gmail.com>
2022-10-04 00:53:37 +0300

    Trainer

--- TypeTrainer/TypeTrainer/MainWindow.xaml.cs
+++ TypeTrainer/TypeTrainer/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -25,20 +26,71 @@ namespace TypeTrainer
             InitializeComponent();
         }
 
-        public event EventHandler<EventArgs> keyDown;
-        
+        UIElementCollection KeyboardKeys;
+        List<Brush> colors;
+        DispatcherTimer timer = new();
 
-        private void Button_KeyDown(object sender, KeyEventArgs e)
+        int CharCounter = 0;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show(sender .ToString() +"K "+ e.Key.ToString());
+            colors = new();
+            KeyboardKeys = KeyBoard.Children;
+
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
+            foreach (Border control in KeyboardKeys)
+            {
+                colors.Add((control.Child as TextBlock).Background.Clone());
+            }
         }
 
-        private void TextBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        private void Timer_Tick(object sender, EventArgs e)
         {
-            MessageBox.Show(sender .ToString() +"P "+ e.Key.ToString());
-
+            TextCPS.Text = "CPS: " + CharCounter;
+            CharCounter = 0;
         }
 
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+                                    CharCounter++;
+            foreach (var control in KeyBoard.Children)
+            {
+                var border = control as Border;
 
+                if (border is not null)
+                {
+                    var textBlock = border.Child as TextBlock;
+
+                    if (textBlock is not null)
+                    {
+                        if (textBlock.Tag.ToString() == e.Key.ToString())
+                        {
+                            textBlock.Background = Brushes.Coral;
+
+                            try
+                            {
+                                if (textBlock.Text.ToLower()[0] == TrainingText.Text.ToLower()[0])
+                                {
+                                    TrainingText.Text = TrainingText.Text.Remove(0, 1);
+                                }
+                            }
+                            catch { }
+
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            foreach (Border control in KeyboardKeys)
+            {
+                (control.Child as TextBlock).Background = colors[KeyboardKeys.IndexOf(control)];
+            }
+        }
     }
 }
