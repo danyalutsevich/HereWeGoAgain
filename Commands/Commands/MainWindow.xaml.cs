71173f2631049c084386d44ba9857c6e26bd9f74
danyalutsevich <luchevich31@gmail.com>
2022-10-10 22:05:10 +0300

    Commands

---
+++ Commands/Commands/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace CommandsWPF
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            random = new();
+        }
+
+        private Random random;
+
+        private void RedColor(object sender, ExecutedRoutedEventArgs e)
+        {
+            Button1.Background = Brushes.Red;
+        }
+
+        private void GreenColor(object sender, ExecutedRoutedEventArgs e)
+        {
+            Button1.Background = Brushes.Green;
+        }
+
+        private void BlueColor(object sender, ExecutedRoutedEventArgs e)
+        {
+            Button1.Background = Brushes.Blue;
+        }
+        Control focusedControl;
+
+        private void RandomColor(object sender, ExecutedRoutedEventArgs e)
+        {
+            Control element = (Control)FocusManager.GetFocusedElement(this);
+
+            if (focusedControl != null)
+            {
+                focusedControl.Background = new SolidColorBrush(Color.FromRgb((byte)random.Next(1, 255), (byte)random.Next(1, 255), (byte)random.Next(1, 255)));
+            }
+        }
+
+        private void StackPanel_GotFocus(object sender, RoutedEventArgs e)
+        {
+            focusedControl = e.Source as Control;
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            new Commands.RichTEXTBOXExample().Show();
+        }
+    }
+
+    public class ColorCommands
+    {
+        static ColorCommands()
+        {
+            RedCommand = new RoutedCommand("RedCommand", typeof(ColorCommands));
+            GreenCommand = new RoutedCommand("GreenCommand", typeof(ColorCommands));
+            BlueCommand = new RoutedCommand("BlueCommand", typeof(ColorCommands));
+            RandomCommand = new RoutedCommand("RandomCommand", typeof(ColorCommands));
+        }
+        public static RoutedCommand RedCommand { get; set; }
+        public static RoutedCommand GreenCommand { get; set; }
+        public static RoutedCommand BlueCommand { get; set; }
+        public static RoutedCommand RandomCommand { get; set; }
+    }
+}
---
+++ Commands/Commands/RichTEXTBOXExample.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Commands
+{
+    /// <summary>
+    /// Interaction logic for RichTEXTBOXExample.xaml
+    /// </summary>
+    public partial class RichTEXTBOXExample : Window
+    {
+        public RichTEXTBOXExample()
+        {
+            InitializeComponent();
+        }
+
+     
+
+        private void RedColor(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (!RTB.Selection.IsEmpty)
+            {
+                RTB.Selection.ApplyPropertyValue(TextElement.ForegroundProperty, Brushes.Red);
+            }
+        }
+
+        private void GreenColor(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (!RTB.Selection.IsEmpty)
+            {
+                RTB.Selection.ApplyPropertyValue(TextElement.ForegroundProperty, Brushes.Green);
+            }
+        }
+
+        private void BlueColor(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (!RTB.Selection.IsEmpty)
+            {
+                RTB.Selection.ApplyPropertyValue(TextElement.ForegroundProperty, Brushes.Blue);
+            }
+        }
+
+        private void Bold(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (!RTB.Selection.IsEmpty)
+            {
+                RTB.Selection.ApplyPropertyValue(TextElement.FontWeightProperty,FontWeights.Bold );
+            }
+        }
+
+        private void Italic(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (!RTB.Selection.IsEmpty)
+            {
+                RTB.Selection.ApplyPropertyValue(TextElement.FontStyleProperty, FontStyles.Italic);
+            }
+        }
+        
+        private void Underline(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (!RTB.Selection.IsEmpty)
+            {
+                RTB.Selection.ApplyPropertyValue(TextDecoration.PenProperty, TextDecorations.Underline);
+            }
+        }
+        
+        private void Clear(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (!RTB.Selection.IsEmpty)
+            {
+                RTB.Selection.Text = "";
+            }
+        }
+
+        private void Font15(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (!RTB.Selection.IsEmpty)
+            {
+                RTB.Selection.ApplyPropertyValue(TextElement.FontSizeProperty, 15.0);
+            }
+        }
+        
+        private void Font30(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (!RTB.Selection.IsEmpty)
+            {
+                RTB.Selection.ApplyPropertyValue(TextElement.FontSizeProperty, 30.0);
+            }
+        }
+    }
+
+
+    public static class RTBCommands
+    {
+        static RTBCommands()
+        {
+            bold = new RoutedCommand("bold", typeof(RTBCommands));
+            italic = new RoutedCommand("italic", typeof(RTBCommands));
+            underline = new RoutedCommand("underline", typeof(RTBCommands));
+            clear = new RoutedCommand("clear", typeof(RTBCommands));
+            font15 = new RoutedCommand("font15", typeof(RTBCommands));
+            font30 = new RoutedCommand("font30", typeof(RTBCommands));
+            RedColor = new RoutedCommand("RedColor", typeof(RTBCommands));
+            GreenColor = new RoutedCommand("GreenColor", typeof(RTBCommands));
+            BlueColor = new RoutedCommand("BlueColor", typeof(RTBCommands));
+        }
+        public static RoutedCommand bold { get; set; }
+        public static RoutedCommand italic { get; set; }
+        public static RoutedCommand underline { get; set; }
+        public static RoutedCommand clear { get; set; }
+        public static RoutedCommand font15 { get; set; }
+        public static RoutedCommand font30 { get; set; }
+        public static RoutedCommand RedColor { get; set; }
+        public static RoutedCommand GreenColor { get; set; }
+        public static RoutedCommand BlueColor { get; set; }
+    }
+}
